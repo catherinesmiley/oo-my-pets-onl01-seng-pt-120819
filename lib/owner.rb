d5c03d81b50f6357b81fbf6e599a5601c3460ad1
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-22 23:37:30 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,50 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  
+  attr_reader :name, :species
+  
+  @@all = []
+  @@count = 0 
+  
+  def initialize(name)
+    @name = name 
+    @species = "human"
+    save
+    @@count += 1 
+  end
+  
+  def say_species 
+    "I am a #{species}."
+  end
+  
+  def save
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.count 
+    @@count 
+  end
+  
+  def self.reset_all
+    @@count = 0 
+  end 
+  
+  def cats 
+    Cat.all.select {|cat| cat.owner == self}
+  end
+  
+  def dogs 
+    Dog.all.select {|dog| dog.owner == self}
+  end
+  
+  def buy_cat(cat_name)
+    binding.pry
+    cats[:name] << Cat.new(cat_name)
+  end
+  
 end
